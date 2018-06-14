cff458ec57a8c7d2bc590a233e939be84b8aa9d0
Scott Bender <scott@scottbender.net>
2018-06-14 17:10:10 -0400

    fix: make order test handle meta deltas

--- test/deltacache.js
+++ test/deltacache.js
@@ -61,11 +61,184 @@ const testDelta = {
 }
 
 const expectedOrder = [
-  '',
-  'navigation.speedOverGround',
-  'navigation.courseOverGroundTrue',
-  'navigation.log',
-  'navigation.trip.log'
+  {
+    context:
+      'vessels.urn:mrn:signalk:uuid:2204ae24-c944-5ffe-8d1d-4d411c9cea2e',
+    updates: [
+      {
+        $source: 'deltaFromHttp',
+        timestamp: '2014-05-03T09:14:11.096Z',
+        values: [
+          {
+            path: '',
+            value: {
+              name: 'TestBoat'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    context:
+      'vessels.urn:mrn:signalk:uuid:2204ae24-c944-5ffe-8d1d-4d411c9cea2e',
+    updates: [
+      {
+        $source: 'schema',
+        timestamp: '2014-05-03T09:14:11.097Z',
+        meta: [
+          {
+            path: 'navigation.speedOverGround',
+            value: {
+              units: 'm/s',
+              description:
+                "Vessel speed over ground. If converting from AIS 'HIGH' value, set to 102.2 (Ais max value) and add warning in notifications"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    context:
+      'vessels.urn:mrn:signalk:uuid:2204ae24-c944-5ffe-8d1d-4d411c9cea2e',
+    updates: [
+      {
+        $source: 'deltaFromHttp',
+        timestamp: '2014-05-03T09:14:11.097Z',
+        values: [
+          {
+            path: 'navigation.speedOverGround',
+            value: 3.85
+          }
+        ]
+      }
+    ]
+  },
+  {
+    context:
+      'vessels.urn:mrn:signalk:uuid:2204ae24-c944-5ffe-8d1d-4d411c9cea2e',
+    updates: [
+      {
+        $source: 'schema',
+        timestamp: '2014-05-03T09:14:11.098Z',
+        meta: [
+          {
+            path: 'navigation.courseOverGroundTrue',
+            value: {
+              units: 'rad',
+              description: 'Course over ground (true)'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    context:
+      'vessels.urn:mrn:signalk:uuid:2204ae24-c944-5ffe-8d1d-4d411c9cea2e',
+    updates: [
+      {
+        $source: 'deltaFromHttp',
+        timestamp: '2014-05-03T09:14:11.098Z',
+        values: [
+          {
+            path: 'navigation.courseOverGroundTrue',
+            value: 172.9
+          }
+        ]
+      }
+    ]
+  },
+  {
+    context:
+      'vessels.urn:mrn:signalk:uuid:2204ae24-c944-5ffe-8d1d-4d411c9cea2e',
+    updates: [
+      {
+        $source: 'schema',
+        timestamp: '2014-05-03T09:14:11.099Z',
+        meta: [
+          {
+            path: 'navigation.log',
+            value: {
+              units: 'm',
+              description: 'Total distance traveled'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    context:
+      'vessels.urn:mrn:signalk:uuid:2204ae24-c944-5ffe-8d1d-4d411c9cea2e',
+    updates: [
+      {
+        $source: 'deltaFromHttp',
+        timestamp: '2014-05-03T09:14:11.099Z',
+        values: [
+          {
+            path: 'navigation.log',
+            value: 17404540
+          }
+        ]
+      }
+    ]
+  },
+  {
+    context:
+      'vessels.urn:mrn:signalk:uuid:2204ae24-c944-5ffe-8d1d-4d411c9cea2e',
+    updates: [
+      {
+        $source: 'schema',
+        timestamp: '2014-05-03T09:14:11.100Z',
+        meta: [
+          {
+            path: 'navigation.trip.log',
+            value: {
+              units: 'm',
+              description:
+                'Total distance traveled on this trip / since trip reset'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    context:
+      'vessels.urn:mrn:signalk:uuid:2204ae24-c944-5ffe-8d1d-4d411c9cea2e',
+    updates: [
+      {
+        $source: 'deltaFromHttp',
+        timestamp: '2014-05-03T09:14:11.100Z',
+        values: [
+          {
+            path: 'navigation.trip.log',
+            value: 43374
+          }
+        ]
+      }
+    ]
+  },
+  {
+    context:
+      'vessels.urn:mrn:signalk:uuid:2204ae24-c944-5ffe-8d1d-4d411c9cea2e',
+    updates: [
+      {
+        $source: 'defaults',
+        timestamp: '2018-06-14T18:19:39.083Z',
+        values: [
+          {
+            path: '',
+            value: {
+              uuid: 'urn:mrn:signalk:uuid:2204ae24-c944-5ffe-8d1d-4d411c9cea2e'
+            }
+          }
+        ]
+      }
+    ]
+  }
 ]
 
 describe('deltacache', () => {
@@ -120,7 +293,15 @@ describe('deltacache', () => {
         var deltas = server.app.deltaCache.getCachedDeltas(null, delta => true)
         assert(deltas.length == expectedOrder.length)
         for (var i = 0; i < expectedOrder.length; i++) {
-          deltas[i].updates[0].values[0].path.should.equal(expectedOrder[i])
+          if (deltas[i].updates[0].values) {
+            deltas[i].updates[0].values[0].path.should.equal(
+              expectedOrder[i].updates[0].values[0].path
+            )
+          } else {
+            deltas[i].updates[0].meta[0].path.should.equal(
+              expectedOrder[i].updates[0].meta[0].path
+            )
+          }
         }
       })
     })
